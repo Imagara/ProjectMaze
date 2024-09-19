2f56cbcaa89a5a4cac3a6bdfcadb07a8933d97e5
Imagara!? <milick33@gmail.com>
2024-09-20 06:13:50 +0700

    MainWindow генерация лабиринта перенесена => MazeGenerator

--- ProjectMaze/MainWindow.xaml.cs
+++ ProjectMaze/MainWindow.xaml.cs
@@ -17,6 +17,8 @@ namespace ProjectMaze
         Player player { get; set; }
         //Таймер для движения игрока без повторного нажатия на клавишу передвижения
         DispatcherTimer pressedKeyTimer { get; set; }
+
+        MazeGenerator MazeGenerator { get; set; }
         //Нажатая клавиша
         Key currentKey = Key.None;
         #region Maze settings
@@ -61,215 +63,33 @@ namespace ProjectMaze
             }
         }
         #endregion
+
         public MainWindow()
         {
             InitializeComponent();
             this.DataContext = this;
         }
-        public Cell GenerateRandomEmptyCell(Cell[,] mapArray, int columnsCount = 0, int rowsCount = 0)
-        {
-            Random rnd = new Random();
-            int x, y;
 
-
-            if (columnsCount == 0 || rowsCount == 0)
-            {
-                columnsCount = _columnsCount;
-                rowsCount = _rowsCount;
-            }
-
-            do // Нахождение свободной ячейки
-            {
-                x = rnd.Next(0, columnsCount - 1);
-                if (x % 2 != 0)
-                    x++;
-                y = rnd.Next(0, rowsCount - 1);
-                if (y % 2 != 0)
-                    y++;
-            } while (mapArray[x, y] is not Space);
-
-            Cell emptyCell = new Space(x, y);
-            return emptyCell;
-        }
-        public List<Cell> GetNeighbours(Cell cell, int width, int height, Cell[,] mapArray, bool isVisitedCheck = true)
-        {
-            //Дистанция ходьбы
-            int walkDist = 2;
-
-            //Расположение клетки
-            int x = cell.x;
-            int y = cell.y;
-
-            //Соседние клетки
-            Cell left = new Space(x - walkDist, y);
-            Cell up = new Space(x, y - walkDist);
-            Cell right = new Space(x + walkDist, y);
-            Cell down = new Space(x, y + walkDist);
-
-            List<Cell> nlist = [left, up, right, down];
-            List<Cell> newlist = new();
-
-            foreach (Cell n in nlist)
-            {
-                if (n.x >= 0 && n.x < width && n.y >= 0 && n.y < height) // Если в пределах лабиринта
-                {
-                    //Проверка была ли посещена клетка
-                    if (isVisitedCheck && (mapArray[n.x, n.y] == null || !mapArray[n.x, n.y].IsVisited))
-                        newlist.Add(n);
-                    //Без проверки
-                    else if (!isVisitedCheck)
-                        newlist.Add(n);
-                }
-            }
-            return newlist;
-        }
-        public Cell GetWallBetweenCells(Cell first, Cell second)
-        {
-            //Получение клетки между двумя другими
-            int x, y;
-            x = second.x - first.x;
-            y = second.y - first.y;
-            x /= 2; y /= 2;
-            Cell cell = new Space(first.x + x, first.y + y);
-            return cell;
-        }
-        public Cell GetRandomUnVisitedCell(Cell[,] mapArray, int columnsCount = 0, int rowsCount = 0)
-        {
-            //Получение случайной не посещенной клетки
-            for (int i = 0; i < columnsCount; i += 2)
-            {
-                for (int j = 0; j < rowsCount; j += 2)
-                {
-                    if (mapArray[i, j] == null)
-                        return new Space(i, j);
-                }
-            }
-            return null;
-        }
-        public void AddToTraces(Cell cell, List<Cell> traces)
-        {
-            //Добавление в список с историей пройденных клеток
-            traces.Add(cell);
-            //Если список слишком большой - удаляется первая клетка в списке
-            if (traces.Count > _rowsCount / 2 * _columnsCount / 2)
-                traces.RemoveAt(0);
-        }
-        public Cell MoveBack(List<Cell> traces)
-        {
-            //Передвижение на прошлую клетку (из списка с историей пройденных клеток)
-            Cell cell = traces.Last();
-            traces.RemoveAt(traces.Count() - 1);
-            return cell;
-        }
-        private int GetAllPointsCount()
-        {
-            //Получение количества семян, которые должны будут сгенерированы(или их общее количество) на карте
-            if (TurnSeedCheckBox.IsChecked == false)//Если семена отключены 
-                return 0;
-            return Convert.ToInt32(1 + ((DifficultySelectedIndex + 1) * 0.75 * ((ColumnsCount + RowsCount) / 5)));
-        }
         private void GetGeneratedMap(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine($"\n\n\nГенерация...");
-            //Определение размеров лабиринта
-            int rows = _rowsCount, columns = _columnsCount;
-            Console.WriteLine($"Размер лабиринта = {RowsCount}x{ColumnsCount}");
-
-            List<Cell> traces = new();
-            Cell[,] mapArray = new Cell[columns, rows];
-            mapCells = new ObservableCollection<ObservableCollection<Cell>>();
-
-            //Стартовая точка генерации
-            Cell startCell = new Space(0, 0, true);
-            mapArray[startCell.x, startCell.y] = startCell;
-            Console.WriteLine($"Стартовая точка генерации: [{startCell.x}][{startCell.y}]");
-            Cell currentCell = startCell;
-            Random rnd = new Random();
-            bool isRandomGenerated = false;
-
-            List<Cell> neighbours;
-            do
-            {
-                if (isRandomGenerated)//Если следующая ячейка генерируется случайно
-                {
-                    neighbours = GetNeighbours(currentCell, columns, rows, mapArray, !isRandomGenerated);
-                    isRandomGenerated = false;
-                }
-                else
-                {
-                    neighbours = GetNeighbours(currentCell, columns, rows, mapArray);
-                }
-
-                if (neighbours.Count() != 0) //Если есть соседние клетки
-                {
-                    int rand = rnd.Next(neighbours.Count());
-                    Cell neighbourCell = neighbours[rand];
-                    neighbourCell.IsVisited = true;
-                    mapArray[neighbourCell.x, neighbourCell.y] = neighbourCell;
-                    Console.WriteLine($"Переход на точку: [{neighbourCell.x}][{neighbourCell.y}]");
-
-                    //Удаление стены между новой и прошлой клеткой
-                    Cell wall = GetWallBetweenCells(currentCell, neighbourCell);
-                    mapArray[wall.x, wall.y] = wall;
-
-                    //Добавление клетки в список с историей ходов
-                    AddToTraces(currentCell, traces);
-                    currentCell = neighbourCell;
-                }
-                else if (neighbours.Count() == 0 && traces.Count > 0 && currentCell != traces.First()) //Если нет соседей и можно вернутся назад
-                {
-                    //Ход назад
-                    currentCell = MoveBack(traces);
-                    Console.WriteLine($"Возврат на [{currentCell.x}][{currentCell.y}]");
-                }
-                else if (GetRandomUnVisitedCell(mapArray) != null)
-                {
-                    //Генерация следующей клетки случайно
-                    currentCell = GetRandomUnVisitedCell(mapArray);
-                    Console.WriteLine($"Переход к следующей случайной клетке: [{currentCell.x}][{currentCell.y}]");
-                    isRandomGenerated = true;
-                }
-                else
-                {
-                    //Лабиринт сгенерирован, выход из цикла
-                    Console.WriteLine($"Все клетки посещены. Лабиринт сгенерирован");
-                    break;
-                }
-            } while (true);
+            MazeGenerator = new(_rowsCount,_columnsCount, TurnSeedCheckBox.IsChecked, _difficultySelectedIndex);
+            Cell[,] mapArray = MazeGenerator.GetGeneratedMap();
 
             #region Размещение игрока
-            Cell playerRandomCell = GenerateRandomEmptyCell(mapArray);
+            Cell playerRandomCell = MazeGenerator.GenerateRandomEmptyCell(mapArray);
             player = new Player(playerRandomCell.x, playerRandomCell.y);
             mapArray[playerRandomCell.x, playerRandomCell.y] = player;
             Console.WriteLine($"Позиция игрока: [{playerRandomCell.x}][{playerRandomCell.y}]");
             RightBorder.DataContext = player;
             #endregion
 
-            #region Создание выхода
-            Cell exitRandomCell = GenerateRandomEmptyCell(mapArray);
-            Exit exit = new Exit(exitRandomCell.x, exitRandomCell.y);
-            mapArray[exitRandomCell.x, exitRandomCell.y] = exit;
-            Console.WriteLine($"Выход был создан [{exitRandomCell.x}][{exitRandomCell.y}]");
-            #endregion
 
-            #region Создание Seeds
-            for (int i = 0; i < GetAllPointsCount(); i++)
-            {
-                Cell randomCell = GenerateRandomEmptyCell(mapArray);
-                if (randomCell == null)
-                    break;
-                Point point = new Point(randomCell.x, randomCell.y);
-                mapArray[randomCell.x, randomCell.y] = point;
-                Console.WriteLine($"Семечко было создано [{randomCell.x}][{randomCell.y}]");
-            }
-            Console.WriteLine($"Всего семян было создано: {GetAllPointsCount()}");
-            #endregion
+            mapCells = new ObservableCollection<ObservableCollection<Cell>>();
 
-
-            for (int j = 0; j < rows; j++)
+            for (int j = 0; j < _rowsCount; j++)
             {
                 mapCells.Add(new ObservableCollection<Cell>());
-                for (int i = 0; i < columns; i++)
+                for (int i = 0; i < _columnsCount; i++)
                 {
                     if (mapArray[i, j] != null)
                         mapCells[j].Add(mapArray[i, j]);
@@ -376,7 +196,7 @@ namespace ProjectMaze
                 target = null;
             }
 
-            if (target is Exit && GetAllPointsCount() <= player.Points)
+            if (target is Exit && MazeGenerator.GetAllPointsCount() <= player.Points)
             {
                 Dispatcher.BeginInvoke(new Action(() =>
                 {
---
+++ ProjectMaze/MazeGenerator.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
+using System.Windows;
+
+namespace ProjectMaze
+{
+    public class MazeGenerator
+    {
+        //Игровое поле
+        ObservableCollection<ObservableCollection<Cell>> mapCells;
+        //Таймер для движения игрока без повторного нажатия на клавишу передвижения
+        DispatcherTimer pressedKeyTimer { get; set; }
+        //Нажатая клавиша
+        Key currentKey = Key.None;
+        #region Maze settings
+
+        private int rows, columns, difficultyIndex;
+        private bool? isTurnSeedCheckBoxChecked;
+        #endregion
+
+        public MazeGenerator(int rows, int columns, bool? isTurnSeedCheckBoxChecked, int difficultyIndex = 0)
+        {
+            this.rows = rows;
+            this.columns= columns;
+            this.isTurnSeedCheckBoxChecked = isTurnSeedCheckBoxChecked;
+            this.difficultyIndex = difficultyIndex;
+        }
+
+        public Cell GenerateRandomEmptyCell(Cell[,] mapArray, int columnsCount = 0, int rowsCount = 0)
+        {
+            Random rnd = new Random();
+            int x, y;
+
+
+            if (columnsCount == 0 || rowsCount == 0)
+            {
+                columnsCount = rows;
+                rowsCount = rows;
+            }
+
+            do // Нахождение свободной ячейки
+            {
+                x = rnd.Next(0, columnsCount - 1);
+                if (x % 2 != 0)
+                    x++;
+                y = rnd.Next(0, rowsCount - 1);
+                if (y % 2 != 0)
+                    y++;
+            } while (mapArray[x, y] is not Space);
+
+            Cell emptyCell = new Space(x, y);
+            return emptyCell;
+        }
+        public List<Cell> GetNeighbours(Cell cell, int width, int height, Cell[,] mapArray, bool isVisitedCheck = true)
+        {
+            //Дистанция ходьбы
+            int walkDist = 2;
+
+            //Расположение клетки
+            int x = cell.x;
+            int y = cell.y;
+
+            //Соседние клетки
+            Cell left = new Space(x - walkDist, y);
+            Cell up = new Space(x, y - walkDist);
+            Cell right = new Space(x + walkDist, y);
+            Cell down = new Space(x, y + walkDist);
+
+            List<Cell> nlist = [left, up, right, down];
+            List<Cell> newlist = new();
+
+            foreach (Cell n in nlist)
+            {
+                if (n.x >= 0 && n.x < width && n.y >= 0 && n.y < height) // Если в пределах лабиринта
+                {
+                    //Проверка была ли посещена клетка
+                    if (isVisitedCheck && (mapArray[n.x, n.y] == null || !mapArray[n.x, n.y].IsVisited))
+                        newlist.Add(n);
+                    //Без проверки
+                    else if (!isVisitedCheck)
+                        newlist.Add(n);
+                }
+            }
+            return newlist;
+        }
+        public Cell GetWallBetweenCells(Cell first, Cell second)
+        {
+            //Получение клетки между двумя другими
+            int x, y;
+            x = second.x - first.x;
+            y = second.y - first.y;
+            x /= 2; y /= 2;
+            Cell cell = new Space(first.x + x, first.y + y);
+            return cell;
+        }
+        public int GetAllPointsCount()
+        {
+            //Получение количества семян, которые должны будут сгенерированы(или их общее количество) на карте
+            if (isTurnSeedCheckBoxChecked == false)//Если семена отключены 
+                return 0;
+            return Convert.ToInt32(1 + ((difficultyIndex + 1) * 0.75 * ((columns + rows) / 5)));
+        }
+        public Cell GetRandomUnVisitedCell(Cell[,] mapArray, int columnsCount = 0, int rowsCount = 0)
+        {
+            //Получение случайной не посещенной клетки
+            for (int i = 0; i < columnsCount; i += 2)
+            {
+                for (int j = 0; j < rowsCount; j += 2)
+                {
+                    if (mapArray[i, j] == null)
+                        return new Space(i, j);
+                }
+            }
+            return null;
+        }
+        public void AddToTraces(Cell cell, List<Cell> traces)
+        {
+            //Добавление в список с историей пройденных клеток
+            traces.Add(cell);
+            //Если список слишком большой - удаляется первая клетка в списке
+            if (traces.Count > rows / 2 * columns / 2)
+                traces.RemoveAt(0);
+        }
+        public Cell MoveBack(List<Cell> traces)
+        {
+            //Передвижение на прошлую клетку (из списка с историей пройденных клеток)
+            Cell cell = traces.Last();
+            traces.RemoveAt(traces.Count() - 1);
+            return cell;
+        }
+
+        public Cell[,] GetGeneratedMap()
+        {
+            Console.WriteLine($"\n\n\nГенерация...");
+
+            Console.WriteLine($"Размер лабиринта = {rows}x{columns}");
+
+            List<Cell> traces = new();
+            Cell[,] mapArray = new Cell[columns, rows];
+
+            //Стартовая точка генерации
+            Cell startCell = new Space(0, 0, true);
+            mapArray[startCell.x, startCell.y] = startCell;
+            Console.WriteLine($"Стартовая точка генерации: [{startCell.x}][{startCell.y}]");
+            Cell currentCell = startCell;
+            Random rnd = new Random();
+            bool isRandomGenerated = false;
+
+            List<Cell> neighbours;
+            do
+            {
+                if (isRandomGenerated)//Если следующая ячейка генерируется случайно
+                {
+                    neighbours = GetNeighbours(currentCell, columns, rows, mapArray, !isRandomGenerated);
+                    isRandomGenerated = false;
+                }
+                else
+                {
+                    neighbours = GetNeighbours(currentCell, columns, rows, mapArray);
+                }
+
+                if (neighbours.Count() != 0) //Если есть соседние клетки
+                {
+                    int rand = rnd.Next(neighbours.Count());
+                    Cell neighbourCell = neighbours[rand];
+                    neighbourCell.IsVisited = true;
+                    mapArray[neighbourCell.x, neighbourCell.y] = neighbourCell;
+                    Console.WriteLine($"Переход на точку: [{neighbourCell.x}][{neighbourCell.y}]");
+
+                    //Удаление стены между новой и прошлой клеткой
+                    Cell wall = GetWallBetweenCells(currentCell, neighbourCell);
+                    mapArray[wall.x, wall.y] = wall;
+
+                    //Добавление клетки в список с историей ходов
+                    AddToTraces(currentCell, traces);
+                    currentCell = neighbourCell;
+                }
+                else if (neighbours.Count() == 0 && traces.Count > 0 && currentCell != traces.First()) //Если нет соседей и можно вернутся назад
+                {
+                    //Ход назад
+                    currentCell = MoveBack(traces);
+                    Console.WriteLine($"Возврат на [{currentCell.x}][{currentCell.y}]");
+                }
+                else if (GetRandomUnVisitedCell(mapArray) != null)
+                {
+                    //Генерация следующей клетки случайно
+                    currentCell = GetRandomUnVisitedCell(mapArray);
+                    Console.WriteLine($"Переход к следующей случайной клетке: [{currentCell.x}][{currentCell.y}]");
+                    isRandomGenerated = true;
+                }
+                else
+                {
+                    //Лабиринт сгенерирован, выход из цикла
+                    Console.WriteLine($"Все клетки посещены. Лабиринт сгенерирован");
+                    break;
+                }
+            } while (true);
+
+            #region Создание выхода
+            Cell exitRandomCell = GenerateRandomEmptyCell(mapArray);
+            Exit exit = new Exit(exitRandomCell.x, exitRandomCell.y);
+            mapArray[exitRandomCell.x, exitRandomCell.y] = exit;
+            Console.WriteLine($"Выход был создан [{exitRandomCell.x}][{exitRandomCell.y}]");
+            #endregion
+
+            #region Создание Seeds
+            for (int i = 0; i < GetAllPointsCount(); i++)
+            {
+                Cell randomCell = GenerateRandomEmptyCell(mapArray);
+                if (randomCell == null)
+                    break;
+                Point point = new Point(randomCell.x, randomCell.y);
+                mapArray[randomCell.x, randomCell.y] = point;
+                Console.WriteLine($"Семечко было создано [{randomCell.x}][{randomCell.y}]");
+            }
+            Console.WriteLine($"Всего семян было создано: {GetAllPointsCount()}");
+            #endregion
+
+            return mapArray;
+        }
+    }
+}
